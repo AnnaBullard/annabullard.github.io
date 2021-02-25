9cb70c64d87f387dd40130c73de5f629ffb0e0dd
AnnaBullard <annabullard89@gmail.com>
2021-02-25 00:57:33 -0800

    slider

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 window.addEventListener("DOMContentLoaded", ()=>{
+    
+    //NAVIGATION
+    let navbar = document.querySelector("#navbar");
+    let main = document.querySelector("main");
+
+    console.log(navbar)
+    navbar.addEventListener("click", (e) => {
+        if (e.target.classList.contains("nav-link")) {
+            let className = e.target.id;
+            main.setAttribute("class", className);
+        }
+    })
+
+    //HOMEPAGE
     const softwareEngineer = {
         about: "I came a long way to realize that programming is my one true passion.\nIt allows me to express my creativity, make other's life easier and my life - exciting.",
         avalableForHire: true,
-        // navigation: ["skills","projects","contacts","timeline"],
-        mission: "make the world a nicer place to live",
         like: ["coding", "JavaScript", "Star Wars"],
+        mission: "Make the world a nicer, life easier and people happier.",
     }
     
     let parent = document.querySelector(".nav-list");
@@ -14,30 +27,12 @@ window.addEventListener("DOMContentLoaded", ()=>{
     
         let listItem = document.createElement("li");
         if (typeof value === "string") {
-            listItem.innerHTML = `<span class="variable">${key}:</span> <span class="string">"${value}"</span>`;
+            listItem.innerHTML = `<span class="variable">${key}:</span> <span class="string">"${value}"</span>,`;
         } else if (typeof value === "boolean") {
-            listItem.innerHTML = `<span class="variable">${key}:</span> <span class="keyword">${value?"true":"false"}</span>`;
-        } else if (Object.prototype.toString.call(value) === "[object Array]" && key === "navigation") {
-            let newList = document.createElement("ul");
-            value.forEach(link => {
-                let newListItem = document.createElement("li");
-                newListItem.innerHTML = `<span class="variable">${link}:</span> `;
-                let button = document.createElement("span");
-                button.setAttribute("class","link");
-                button.setAttribute("id",link);
-                button.innerHTML = "[...]";
-                button.addEventListener("Click", (e) => {console.log(link)})
-                newListItem.appendChild(button);
-                newList.appendChild(newListItem);
-            })
-            listItem.innerHTML = `<span class="variable">${key}:</span> {`
-            listItem.appendChild(newList);
-            listItem.innerHTML += `}`
+            listItem.innerHTML = `<span class="variable">${key}:</span> <span class="keyword">${value?"true":"false"}</span>,`;
         } else if (Object.prototype.toString.call(value) === "[object Array]") {
-            listItem.innerHTML = `<span class="variable">${key}:</span> [ <span class="string">"${value.join('"</span>, <span class="string">"')}"]</span>`;
-        } else {
-            console.log({type: Object.prototype.toString.call(value)})
-        }
+            listItem.innerHTML = `<span class="variable">${key}:</span> [ <span class="string">"${value.join('"</span>, <span class="string">"')}"</span>] ,`;
+        } 
         parent.appendChild(listItem);
     }
 })
